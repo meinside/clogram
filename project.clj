47a7d4d2b070d97023bd079b0946868aea9ea8da
Sungjin Han <meinside@gmail.com>
2019-12-09 12:15:42 +0900

    Initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject clogram "0.0.0"
+  :description "A Telegram Bot Library for Clojure"
+  :url "https://github.com/meinside/clogram"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [clj-http "3.10.0"]
+                 [org.clojure/data.json "0.2.7"]]
+  :repl-options {:init-ns clogram.bot})
