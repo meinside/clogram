feb93b88c5d4ece8572f219e644cc1183b2b08e2
Sungjin Han <meinside@gmail.com>
2019-12-13 11:34:44 +0900

    Edit files for tests

---
+++ test/meinside/clogram_test.clj
@@ -0,0 +1,140 @@
+(ns meinside.clogram-test
+  (:require [clojure.test :refer :all]
+            [meinside.clogram :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
+
+;; TODO - new-bot
+
+;; TODO - delete-webhook
+
+;; TODO - get-me
+
+;; TODO - get-updates
+
+;; TODO - poll-updates
+
+;; TODO - stop-polling
+
+;; TODO - send-message
+
+;; TODO - forward-message
+
+;; TODO - send-photo
+
+;; TODO - send-audio
+
+;; TODO - send-document
+
+;; TODO - send-sticker
+
+;; TODO - get-sticker-set
+
+;; TODO - upload-sticker-file
+
+;; TODO - create-new-sticker-set
+
+;; TODO - add-sticker-to-set
+
+;; TODO - set-sticker-position-in-set
+
+;; TODO - delete-sticker-from-set
+
+;; TODO - send-video
+
+;; TODO - send-animation
+
+;; TODO - send-voice
+
+;; TODO - send-video-note
+
+;; TODO - send-media-group
+
+;; TODO - send-location
+
+;; TODO - send-venue
+
+;; TODO - send-contact
+
+;; TODO - send-poll
+
+;; TODO - stop-poll
+
+;; TODO - send-chat-action
+
+;; TODO - get-user-profile-photos
+
+;; TODO - get-file-url
+
+;; TODO - get-file
+
+;; TODO - kick-chat-member
+
+;; TODO - leave-chat
+
+;; TODO - unban-chat-member
+
+;; TODO - restrict-chat-member
+
+;; TODO - promote-chat-member
+
+;; TODO - set-chat-permission
+
+;; TODO - export-chat-invite-link
+
+;; TODO - set-chat-photo
+
+;; TODO - delete-chat-photo
+
+;; TODO - set-chat-title
+
+;; TODO - set-chat-description
+
+;; TODO - pin-chat-message
+
+;; TODO - unpin-chat-message
+
+;; TODO - get-chat
+
+;; TODO - get-chat-administrators
+
+;; TODO - get-chat-members-count
+
+;; TODO - get-chat-member
+
+;; TODO - set-chat-sticker-set
+
+;; TODO - delete-chat-sticker-set
+
+;; TODO - answer-callback-query
+
+;; TODO - edit-message-text
+
+;; TODO - edit-message-caption
+
+;; TODO - edit-message-media
+
+;; TODO - edit-message-reply-markup
+
+;; TODO - edit-message-live-location
+
+;; TODO - stop-message-live-location
+
+;; TODO - delete-message
+
+;; TODO - answer-inline-query
+
+;; TODO - send-invoice
+
+;; TODO - answer-shipping-query
+
+;; TODO - answer-pre-checkout-query
+
+;; TODO - send-game
+
+;; TODO - set-game-score
+
+;; TODO - get-game-highscores
+
